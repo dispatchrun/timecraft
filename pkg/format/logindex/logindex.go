b2dd25a57d986b9314a6e90de08a76b63edca81b
Achille Roussel <achille.roussel@gmail.com>
2023-04-12 11:51:14 -0600

    add generic logindex format
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

---
+++ pkg/format/logindex/logindex.go
@@ -0,0 +1,3 @@
+// Package logindex contains the definitions of the flatbuffers file format for
+// log indexes.
+package logindex
--- pkg/format/offsetindex/offsetindex.go
+++
@@ -1,3 +0,0 @@
-// Package offsetindex contains the definitions of the flatbuffers file format
-// for log offset indexes.
-package offsetindex
