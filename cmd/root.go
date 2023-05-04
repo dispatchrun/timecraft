d70f7146612841e7875900f3bf6131624ef9d93f
Achille Roussel <achille.roussel@gmail.com>
2023-05-03 17:04:59 -0700

    use stealthrocket/wasi
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,19 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "timecraft",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A time machine for production",
+	Long:  ``,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,15 +26,14 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.timecraft.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func cmdFunc(fn func(context.Context, []string) error) func(*cobra.Command, []string) {
+	return func(cmd *cobra.Command, args []string) {
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer cancel()
 
+		err := fn(ctx, args)
+		cobra.CheckErr(err)
+	}
+}
--- cmd/run.go
+++ cmd/run.go
@@ -1,40 +1,145 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"context"
+	"crypto/rand"
 	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/stealthrocket/wasi"
+	"github.com/stealthrocket/wasi/imports/wasi_snapshot_preview1"
+	"github.com/stealthrocket/wasi/wasiunix"
+	"github.com/stealthrocket/wazergo"
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/sys"
 )
 
-// runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
-	},
+	Short: "Run a WebAssembly module",
+	Long:  `TODO`,
+	Args:  cobra.MatchAll(cobra.MinimumNArgs(1), cobra.OnlyValidArgs),
+	Run:   cmdFunc(run),
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func run(ctx context.Context, args []string) error {
+	wasmFile := args[0]
+	wasmName := filepath.Base(wasmFile)
+	wasmCode, err := os.ReadFile(wasmFile)
+	if err != nil {
+		return fmt.Errorf("could not read WASM file '%s': %w", wasmFile, err)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+	environ := os.Environ()
+	if os.Getenv("PWD") == "" {
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		environ = append(environ, "PWD="+wd)
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	provider := &wasiunix.Provider{
+		Args:               append([]string{wasmName}, args[1:]...),
+		Environ:            environ,
+		Realtime:           realtime,
+		RealtimePrecision:  time.Microsecond,
+		Monotonic:          monotonic,
+		MonotonicPrecision: time.Nanosecond,
+		Rand:               rand.Reader,
+		Exit: func(ctx context.Context, exitCode int) error {
+			panic(sys.NewExitError(uint32(exitCode)))
+		},
+	}
+
+	stdin, err := dup(0)
+	if err != nil {
+		return fmt.Errorf("opening stdin: %w", err)
+	}
+
+	stdout, err := dup(1)
+	if err != nil {
+		return fmt.Errorf("opening stdout: %w", err)
+	}
+
+	stderr, err := dup(2)
+	if err != nil {
+		return fmt.Errorf("opening stderr: %w", err)
+	}
+
+	root, err := syscall.Open("/", syscall.O_DIRECTORY, 0)
+	if err != nil {
+		return fmt.Errorf("opening root directory: %w", err)
+	}
+
+	provider.Preopen(stdin, "/dev/stdin", wasi.FDStat{
+		FileType:   wasi.CharacterDeviceType,
+		RightsBase: wasi.AllRights,
+	})
+
+	provider.Preopen(stdout, "/dev/stdout", wasi.FDStat{
+		FileType:   wasi.CharacterDeviceType,
+		RightsBase: wasi.AllRights,
+	})
+
+	provider.Preopen(stderr, "/dev/stderr", wasi.FDStat{
+		FileType:   wasi.CharacterDeviceType,
+		RightsBase: wasi.AllRights,
+	})
+
+	provider.Preopen(root, "/", wasi.FDStat{
+		FileType:         wasi.DirectoryType,
+		RightsBase:       wasi.AllRights,
+		RightsInheriting: wasi.AllRights,
+	})
+
+	runtime := wazero.NewRuntime(ctx)
+	defer runtime.Close(ctx)
+
+	ctx = wazergo.WithModuleInstance(ctx,
+		wazergo.MustInstantiate(ctx, runtime,
+			wasi_snapshot_preview1.HostModule,
+			wasi_snapshot_preview1.WithWASI(provider),
+		),
+	)
+
+	instance, err := runtime.Instantiate(ctx, wasmCode)
+	if err != nil {
+		switch e := err.(type) {
+		case *sys.ExitError:
+			if exitCode := e.ExitCode(); exitCode != 0 {
+				os.Exit(int(exitCode))
+			}
+		default:
+			return err
+		}
+	}
+	return instance.Close(ctx)
+}
+
+var epoch = time.Now()
+
+func monotonic(context.Context) (uint64, error) {
+	return uint64(time.Since(epoch)), nil
+}
+
+func realtime(context.Context) (uint64, error) {
+	return uint64(time.Now().UnixNano()), nil
+}
+
+func dup(fd int) (int, error) {
+	newfd, err := syscall.Dup(fd)
+	if err != nil {
+		return -1, err
+	}
+	syscall.CloseOnExec(newfd)
+	return newfd, nil
 }
