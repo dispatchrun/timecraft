513df5cb59756c1eb32cbda577be33977d439572
Chris O'Hara <cohara87@gmail.com>
2023-05-16 09:45:10 +1000

    Setup entrypoint

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/stealthrocket/timecraft/cmd"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+func usage() {
+	fmt.Print(`timecraft - A time machine for production
+
+USAGE:
+   timecraft [OPTIONS]... <COMMAND> [ARGS]...
+
+COMMANDS:
+   record  Record a trace of execution
+   replay  Replay a trace of execution
+
+OPTIONS:
+   -v, --version
+      Print the version and exit
+
+   -h, --help
+      Show this usage information
+`)
+}
 
 func main() {
-	cmd.Execute()
+	root := flag.NewFlagSet("timecraft", flag.ExitOnError)
+	root.Usage = usage
+
+	version := root.Bool("version", false, "")
+	root.BoolVar(version, "v", false, "")
+
+	root.Parse(os.Args[1:])
+
+	if *version {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
+			fmt.Println("timecraft", info.Main.Version)
+		} else {
+			fmt.Println("timecraft", "devel")
+		}
+		os.Exit(0)
+	}
+
+	args := root.Args()
+	if len(args) == 0 {
+		usage()
+		os.Exit(1)
+	}
+	switch args[0] {
+	case "record":
+		// TODO
+	case "replay":
+		// TODO
+	}
 }
