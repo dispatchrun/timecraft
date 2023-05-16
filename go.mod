bf0a136eed493f8c8637f4e0c37b2e66b2def681
Chris O'Hara <cohara87@gmail.com>
2023-05-16 10:08:59 +1000

    Add run command

--- go.mod
+++ go.mod
@@ -6,14 +6,11 @@ require (
 	github.com/google/flatbuffers v23.3.3+incompatible
 	github.com/google/go-cmp v0.5.9
 	github.com/klauspost/compress v1.16.5
-	github.com/spf13/cobra v1.7.0
-	github.com/stealthrocket/wasi-go v0.0.0-20230512014009-0b932f09275d
-	github.com/stealthrocket/wazergo v0.17.1
+	github.com/stealthrocket/wasi-go v0.1.0
 	github.com/tetratelabs/wazero v1.1.0
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stealthrocket/wazergo v0.17.1 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"runtime/debug"
+
+	"github.com/stealthrocket/wasi-go/imports"
+	"github.com/tetratelabs/wazero"
 )
 
-func usage() {
+func main() {
+	if err := root(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func rootUsage() {
 	fmt.Print(`timecraft - A time machine for production
 
 USAGE:
    timecraft [OPTIONS]... <COMMAND> [ARGS]...
 
 COMMANDS:
-   record  Record a trace of execution
-   replay  Replay a trace of execution
+   run  Run a WebAssembly module
 
 OPTIONS:
    -v, --version
@@ -26,14 +37,14 @@ OPTIONS:
 `)
 }
 
-func main() {
-	root := flag.NewFlagSet("timecraft", flag.ExitOnError)
-	root.Usage = usage
+func root(args []string) (err error) {
+	flagSet := flag.NewFlagSet("timecraft", flag.ExitOnError)
+	flagSet.Usage = rootUsage
 
-	version := root.Bool("version", false, "")
-	root.BoolVar(version, "v", false, "")
+	version := flagSet.Bool("version", false, "")
+	flagSet.BoolVar(version, "v", false, "")
 
-	root.Parse(os.Args[1:])
+	flagSet.Parse(args)
 
 	if *version {
 		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
@@ -44,15 +55,131 @@ func main() {
 		os.Exit(0)
 	}
 
-	args := root.Args()
+	args = flagSet.Args()
 	if len(args) == 0 {
-		usage()
+		rootUsage()
 		os.Exit(1)
 	}
 	switch args[0] {
-	case "record":
-		// TODO
-	case "replay":
-		// TODO
+	case "run":
+		err = run(args[1:])
+	default:
+		err = fmt.Errorf("invalid command %q", args[0])
 	}
+	return
+}
+
+func runUsage() {
+	fmt.Print(`timecraft run - Run a WebAssembly module
+
+USAGE:
+   timecraft run [OPTIONS]... <MODULE> [--] [ARGS]...
+
+ARGS:
+   <MODULE>
+      The path of the WebAssembly module to run
+
+   [ARGS]...
+      Arguments to pass to the module
+
+OPTIONS:
+   --dir <DIR>
+      Grant access to the specified host directory
+
+   --listen <ADDR>
+      Grant access to a socket listening on the specified address
+
+   --dial <ADDR>
+      Grant access to a socket connected to the specified address
+
+   --env <NAME=VAL>
+      Pass an environment variable to the module
+
+   --sockets <NAME>
+      Enable a sockets extension, either {none, auto, path_open,
+      wasmedgev1, wasmedgev2}
+
+   --trace
+      Enable logging of system calls (like strace)
+
+   -h, --help
+      Show this usage information
+`)
+}
+
+func run(args []string) error {
+	flagSet := flag.NewFlagSet("timecraft run", flag.ExitOnError)
+	flagSet.Usage = runUsage
+
+	var envs stringList
+	var dirs stringList
+	var listens stringList
+	var dials stringList
+	flagSet.Var(&envs, "env", "")
+	flagSet.Var(&dirs, "dir", "")
+	flagSet.Var(&listens, "listen", "")
+	flagSet.Var(&dials, "dial", "")
+	sockets := flagSet.String("sockets", "auto", "")
+	trace := flagSet.Bool("trace", false, "")
+
+	flagSet.Parse(args)
+
+	args = flagSet.Args()
+	if len(args) == 0 {
+		runUsage()
+		os.Exit(1)
+	}
+
+	wasmFile := args[0]
+	wasmName := filepath.Base(wasmFile)
+	wasmCode, err := os.ReadFile(wasmFile)
+	if err != nil {
+		return fmt.Errorf("could not read WASM file '%s': %w", wasmFile, err)
+	}
+
+	args = args[1:]
+	if len(args) > 0 && args[0] == "--" {
+		args = args[1:]
+	}
+
+	ctx := context.Background()
+	runtime := wazero.NewRuntime(ctx)
+	defer runtime.Close(ctx)
+
+	wasmModule, err := runtime.CompileModule(ctx, wasmCode)
+	if err != nil {
+		return err
+	}
+
+	builder := imports.NewBuilder().
+		WithName(wasmName).
+		WithArgs(args...).
+		WithEnv(envs...).
+		WithDirs(dirs...).
+		WithListens(listens...).
+		WithDials(dials...).
+		WithSocketsExtension(*sockets, wasmModule).
+		WithTracer(*trace, os.Stderr)
+
+	ctx, err = builder.Instantiate(ctx, runtime)
+	if err != nil {
+		return err
+	}
+
+	instance, err := runtime.InstantiateModule(ctx, wasmModule, wazero.NewModuleConfig())
+	if err != nil {
+		return err
+	}
+	return instance.Close(ctx)
+}
+
+type stringList []string
+
+func (s stringList) String() string {
+	return fmt.Sprintf("%v", []string(s))
+}
+
+func (s *stringList) Set(value string) error {
+	*s = append(*s, value)
+	return nil
 }
