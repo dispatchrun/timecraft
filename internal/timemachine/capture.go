64c53b026d66b1a03879d1be7b7f4afc31f8a2a5
Chris O'Hara <cohara87@gmail.com>
2023-05-17 14:46:46 +1000

    Factor out the decorator

--- internal/timemachine/capture.go
+++ internal/timemachine/capture.go
@@ -50,35 +50,3 @@ func Capture[T wazergo.Module](functions FunctionIndex, capture func(Record)) wa
 		}
 	})
 }
-
-// FunctionIndex is a set of functions.
-type FunctionIndex struct {
-	lookup    map[Function]int
-	functions []Function
-}
-
-// Add adds a function to the set.
-func (i *FunctionIndex) Add(moduleName, functionName string) bool {
-	if i.lookup == nil {
-		i.lookup = map[Function]int{}
-	}
-	fn := Function{moduleName, functionName}
-	if _, exists := i.lookup[fn]; exists {
-		return false
-	}
-	i.lookup[fn] = len(i.functions)
-	i.functions = append(i.functions, fn)
-	return true
-}
-
-// Lookup returns the ID associated with a function.
-func (i *FunctionIndex) Lookup(moduleName, functionName string) (int, bool) {
-	fn := Function{Module: moduleName, Name: functionName}
-	id, ok := i.lookup[fn]
-	return id, ok
-}
-
-// Functions is the set of functions.
-func (i *FunctionIndex) Functions() []Function {
-	return i.functions
-}
--- main.go
+++ main.go
@@ -16,10 +16,7 @@ import (
 	"github.com/stealthrocket/wasi-go"
 	"github.com/stealthrocket/wasi-go/imports"
 	"github.com/stealthrocket/wasi-go/imports/wasi_snapshot_preview1"
-	"github.com/stealthrocket/wazergo"
 	"github.com/tetratelabs/wazero"
-	"github.com/tetratelabs/wazero/api"
-	"github.com/tetratelabs/wazero/sys"
 )
 
 var version = "devel"
@@ -372,64 +369,7 @@ func replay(args []string) error {
 
 	records := timemachine.NewLogRecordIterator(logReader)
 
-	builder = builder.WithDecorators(wazergo.DecoratorFunc[*wasi_snapshot_preview1.Module](func(moduleName string, f wazergo.Function[*wasi_snapshot_preview1.Module]) wazergo.Function[*wasi_snapshot_preview1.Module] {
-		functionID, ok := functions.Lookup(moduleName, f.Name)
-		if !ok {
-			return f
-		}
-		return wazergo.Function[*wasi_snapshot_preview1.Module]{
-			Name:    f.Name,
-			Params:  f.Params,
-			Results: f.Results,
-			Func: func(module *wasi_snapshot_preview1.Module, ctx context.Context, mod api.Module, stack []uint64) {
-				// TODO: better error handling
-				if !records.Next() {
-					if err := records.Error(); err != nil {
-						panic(err)
-					}
-					panic("EOF")
-				}
-				record := records.Record()
-				if recordFunctionID := record.Function(); recordFunctionID != functionID {
-					panic(fmt.Sprintf("function ID mismatch: got %d, expect %d", functionID, recordFunctionID))
-				}
-				if len(f.Params) != record.NumParams() {
-					panic(fmt.Sprintf("function param count mismatch: got %d, expect %d", len(f.Params), record.NumParams()))
-				}
-				for i := 0; i < record.NumParams(); i++ {
-					if recordParam := record.ParamAt(i); recordParam != stack[i] {
-						panic(fmt.Sprintf("function param %d mismatch: got %d, expect %d", i, stack[i], recordParam))
-					}
-				}
-				if len(f.Results) != record.NumResults() {
-					panic(fmt.Sprintf("function result count mismatch: got %d, expect %d", len(f.Results), record.NumResults()))
-				}
-
-				memory := mod.Memory()
-				err := record.ScanMemoryAccess(func(m timemachine.MemoryAccess) bool {
-					b, ok := memory.Read(m.Offset, uint32(len(m.Memory)))
-					if !ok {
-						panic(fmt.Sprintf("unable to write %d bytes of memory to offset %d", len(m.Memory), m.Offset))
-					}
-					copy(b, m.Memory)
-					return true
-				})
-				if err != nil {
-					panic(err)
-				}
-
-				for i := 0; i < record.NumResults(); i++ {
-					stack[i] = record.ResultAt(i)
-				}
-
-				// TODO: the record doesn't capture the fact that a host function
-				//  didn't return. Hard-code this case for now.
-				if moduleName == wasi_snapshot_preview1.HostModuleName && f.Name == "proc_exit" {
-					panic(sys.NewExitError(uint32(stack[0])))
-				}
-			},
-		}
-	}))
+	builder = builder.WithDecorators(timemachine.Replay[*wasi_snapshot_preview1.Module](functions, records))
 
 	var system wasi.System
 	ctx, system, err = builder.Instantiate(ctx, runtime)
