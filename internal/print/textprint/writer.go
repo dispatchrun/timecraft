72d16a9f1a65b11b368179915008f56b78d0b870
Achille Roussel <achille.roussel@gmail.com>
2023-05-29 16:02:04 -0700

    add separator between section of text output
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- internal/print/textprint/writer.go
+++ internal/print/textprint/writer.go
@@ -8,16 +8,26 @@ import (
 	"github.com/stealthrocket/timecraft/internal/stream"
 )
 
+const (
+	separator = "--------------------------------------------------------------------------------\n"
+)
+
 func NewWriter[T any](w io.Writer) stream.WriteCloser[T] {
-	return writer[T]{output: bufio.NewWriter(w)}
+	return &writer[T]{output: bufio.NewWriter(w)}
 }
 
 type writer[T any] struct {
 	output *bufio.Writer
+	count  int
 }
 
-func (w writer[T]) Write(values []T) (int, error) {
+func (w *writer[T]) Write(values []T) (int, error) {
 	for n, v := range values {
+		if w.count++; w.count > 1 {
+			if _, err := io.WriteString(w.output, separator); err != nil {
+				return n, err
+			}
+		}
 		if _, err := fmt.Fprint(w.output, v); err != nil {
 			return n, err
 		}
@@ -25,6 +35,6 @@ func (w writer[T]) Write(values []T) (int, error) {
 	return len(values), nil
 }
 
-func (w writer[T]) Close() error {
+func (w *writer[T]) Close() error {
 	return w.output.Flush()
 }
