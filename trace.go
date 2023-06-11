595a03e80726fff34fbaee8683b2df450c07a1f1
Achille Roussel <achille.roussel@gmail.com>
2023-06-10 21:58:35 -0700

    timecraft: add trace subcommands
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- trace.go
+++ trace.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
@@ -14,27 +14,54 @@ import (
 	"github.com/stealthrocket/timecraft/internal/print/yamlprint"
 	"github.com/stealthrocket/timecraft/internal/stream"
 	"github.com/stealthrocket/timecraft/internal/timemachine"
+	"golang.org/x/exp/slices"
 )
 
 const traceUsage = `
-Usage:	timecraft trace [options] <process id>
+Usage:	timecraft trace <layer> <process id> [options]
 
 Options:
    -c, --config             Path to the timecraft configuration file (overrides TIMECRAFTCONFIG)
    -d, --duration duration  Duration of the trace (default to the process uptime)
    -h, --help               Show this usage information
-   -x, --exchange           Show protocol messages exchanged instead of low level connection events
    -o, --output format      Output format, one of: text, json, yaml
    -t, --start-time time    Time at which the trace starts (default to 1 minute)
    -v, --verbose            For text output, display more details about the trace
 `
+
+type layer struct {
+	typ   string
+	alt   []string
+	trace func(io.Writer, outputFormat, string, stream.Reader[tracing.Event]) error
+}
+
+var layers = [...]layer{
+	{
+		typ:   "network",
+		alt:   []string{"net", "nets", "networks"},
+		trace: traceNetwork,
+	},
+	{
+		typ:   "request",
+		alt:   []string{"req", "reqs", "requests"},
+		trace: traceRequest,
+	},
+}
+
+func findLayer(typ string) (*layer, error) {
+	for i, l := range layers {
+		if l.typ == typ || slices.Contains(l.alt, typ) {
+			return &layers[i], nil
+		}
+	}
+	return nil, fmt.Errorf(`no tracing layers matching '%s'`, typ)
+}
 
 func trace(ctx context.Context, args []string) error {
 	var (
 		output    = outputFormat("text")
 		startTime = human.Time{}
 		duration  = human.Duration(1 * time.Minute)
-		exchange  = false
 		verbose   = false
 	)
 
@@ -42,18 +69,27 @@ func trace(ctx context.Context, args []string) error {
 	customVar(flagSet, &output, "o", "output")
 	customVar(flagSet, &duration, "d", "duration")
 	customVar(flagSet, &startTime, "t", "start-time")
-	boolVar(flagSet, &exchange, "x", "exchange")
 	boolVar(flagSet, &verbose, "v", "verbose")
 
 	args, err := parseFlags(flagSet, args)
 	if err != nil {
 		return err
 	}
-	if len(args) != 1 {
-		return errors.New(`expected exactly one process id as argument`)
+	if len(args) != 2 {
+		perrorf(`Expected trace layer and process id as arguments:
+
+   $ timecraft network <process id> ...
+   $ timecraft request <process id> ...
+`)
+		return exitCode(2)
 	}
 
-	processID, err := parseProcessID(args[0])
+	layer, err := findLayer(args[0])
+	if err != nil {
+		perror(err)
+		return exitCode(2)
+	}
+	processID, err := parseProcessID(args[1])
 	if err != nil {
 		return err
 	}
@@ -92,48 +128,51 @@ func trace(ctx context.Context, args []string) error {
 		format = "%+v"
 	}
 
-	events := &tracing.EventReader{
+	return layer.trace(os.Stdout, output, format, &tracing.EventReader{
 		Records: timemachine.NewLogRecordReader(logReader),
-	}
+	})
+}
 
-	if exchange {
-		var writer stream.WriteCloser[tracing.Exchange]
-		switch output {
-		case "json":
-			writer = jsonprint.NewWriter[tracing.Exchange](os.Stdout)
-		case "yaml":
-			writer = yamlprint.NewWriter[tracing.Exchange](os.Stdout)
-		default:
-			writer = textprint.NewWriter[tracing.Exchange](os.Stdout,
-				textprint.Format[tracing.Exchange](format),
-				textprint.Separator[tracing.Exchange]("\n"),
-			)
-			defer fmt.Println()
-		}
-		defer writer.Close()
-		_, err = stream.Copy[tracing.Exchange](writer, &tracing.ExchangeReader{
-			Messages: &tracing.MessageReader{
-				Events: events,
-				Protos: []tracing.ConnProtocol{
-					tracing.HTTP1(),
-				},
-			},
-		})
-	} else {
-		var writer stream.WriteCloser[tracing.Event]
-		switch output {
-		case "json":
-			writer = jsonprint.NewWriter[tracing.Event](os.Stdout)
-		case "yaml":
-			writer = yamlprint.NewWriter[tracing.Event](os.Stdout)
-		default:
-			writer = textprint.NewWriter[tracing.Event](os.Stdout,
-				textprint.Format[tracing.Event](format),
-				textprint.Separator[tracing.Event](""),
-			)
-		}
-		defer writer.Close()
-		_, err = stream.Copy[tracing.Event](writer, events)
+func traceNetwork(w io.Writer, output outputFormat, format string, events stream.Reader[tracing.Event]) error {
+	var writer stream.WriteCloser[tracing.Event]
+	switch output {
+	case "json":
+		writer = jsonprint.NewWriter[tracing.Event](w)
+	case "yaml":
+		writer = yamlprint.NewWriter[tracing.Event](w)
+	default:
+		writer = textprint.NewWriter[tracing.Event](w,
+			textprint.Format[tracing.Event](format),
+			textprint.Separator[tracing.Event](""),
+		)
 	}
+	defer writer.Close()
+	_, err := stream.Copy[tracing.Event](writer, events)
+	return err
+}
+
+func traceRequest(w io.Writer, output outputFormat, format string, events stream.Reader[tracing.Event]) error {
+	var writer stream.WriteCloser[tracing.Exchange]
+	switch output {
+	case "json":
+		writer = jsonprint.NewWriter[tracing.Exchange](w)
+	case "yaml":
+		writer = yamlprint.NewWriter[tracing.Exchange](w)
+	default:
+		writer = textprint.NewWriter[tracing.Exchange](w,
+			textprint.Format[tracing.Exchange](format),
+			textprint.Separator[tracing.Exchange]("\n"),
+		)
+		defer fmt.Println()
+	}
+	defer writer.Close()
+	_, err := stream.Copy[tracing.Exchange](writer, &tracing.ExchangeReader{
+		Messages: &tracing.MessageReader{
+			Events: events,
+			Protos: []tracing.ConnProtocol{
+				tracing.HTTP1(),
+			},
+		},
+	})
 	return err
 }
