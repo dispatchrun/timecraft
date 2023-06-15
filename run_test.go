a17611976419dd2106f640bdf16742744b0aa7ac
Achille Roussel <achille.roussel@gmail.com>
2023-06-15 12:34:26 -0700

    automaticaly run all files named _test.go in testdata/go
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- run_test.go
+++ run_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stealthrocket/timecraft/internal/assert"
@@ -23,27 +24,25 @@ var run = tests{
 		assert.Equal(t, exitCode, 0)
 	},
 
-	"run the testing/fstest test suite": func(t *testing.T) {
-		testRun(t, "testdata/go/fstest_test.wasm", "-path=testdata/fs",
-			// This is the list of files expected to be found in the test directory.
-			"empty",
-			"message",
-			"tmp/file-1",
-			"tmp/file-2",
-		)
-	},
-
-	"run the x/net/nettest test suite": func(t *testing.T) {
-		testRun(t, "testdata/go/nettest_test.wasm")
+	"run Go tests": func(t *testing.T) {
+		files, _ := filepath.Glob("testdata/go/*_test.wasm")
+		if len(files) == 0 {
+			t.Fatal("there are no Go tests to run, this does not seem right")
+		}
+		for _, file := range files {
+			t.Run(file, func(t *testing.T) { testRun(t, file) })
+		}
 	},
 }
 
 func testRun(t *testing.T, module string, args ...string) {
 	command := append([]string{"run", "--", module}, args...)
+
 	stdout, stderr, exitCode := timecraft(t, command...)
 	if exitCode != 0 {
 		fmt.Fprintf(os.Stdout, "=== STDOUT:\n%s", stdout)
 		fmt.Fprintf(os.Stderr, "=== STDERR:\n%s", stderr)
 	}
+
 	assert.Equal(t, exitCode, 0)
 }
--- testdata/go/fstest_test.go
+++ testdata/go/fstest_test.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"testing"
 	"testing/fstest"
 )
 
-var (
-	path string
-	args []string
-)
-
-func TestMain(m *testing.M) {
-	flag.StringVar(&path, "path", ".", "Path to the file system to run the test suite against")
-	flag.Parse()
-	args = flag.Args()
-	os.Exit(m.Run())
-}
-
 func TestFS(t *testing.T) {
-	if err := fstest.TestFS(os.DirFS(path), args...); err != nil {
+	// TODO: fix the current working directory in timecraft tests
+	if err := fstest.TestFS(os.DirFS("testdata/fs"),
+		"empty",
+		"message",
+		"tmp/file-1",
+		"tmp/file-2",
+	); err != nil {
 		t.Error(err)
 	}
 }
