7613079e70b4dca8eff7e9403d11194d11703984
Chris O'Hara <cohara87@gmail.com>
2023-06-22 11:28:57 +1000

    Fix retry

--- internal/timecraft/retry.go
+++ internal/timecraft/retry.go
@@ -6,7 +6,7 @@ import (
 )
 
 func retry(maxAttempts int, minDelay, maxDelay time.Duration, fn func() bool) {
-	for attempt := 1; !fn() && attempt < maxAttempts; attempt++ {
+	for attempt := 1; fn() && attempt < maxAttempts; attempt++ {
 		delay := minDelay * time.Duration(math.Pow(2, float64(attempt-1)))
 		if delay > maxDelay {
 			delay = maxDelay
