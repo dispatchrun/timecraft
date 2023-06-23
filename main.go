eb61cdbe1e39260a4c964111ea5310c19ad289a6
Chris O'Hara <cohara87@gmail.com>
2023-06-24 07:06:14 +1000

    Enable logging

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"context"
-	"io"
-	"log"
 	"os"
 )
-
-func init() {
-	// TODO: do something better with logs
-	log.SetOutput(io.Discard)
-}
 
 func main() {
 	os.Exit(Root(context.Background(), os.Args[1:]...))
