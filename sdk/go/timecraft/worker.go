b725be744c1e3a7b8a8d7a7c45e7ed461f636b50
Chris O'Hara <cohara87@gmail.com>
2023-06-28 13:52:05 +1000

    Create a standard logger for Go

--- sdk/go/timecraft/worker.go
+++ sdk/go/timecraft/worker.go
@@ -11,9 +11,23 @@ import (
 
 // StartWorker starts a timecraft worker.
 func StartWorker(handler http.Handler) error {
+	client, err := NewClient()
+	if err != nil {
+		return err
+	}
+	logger := client.Logger()
+	logger.Printf("starting worker")
+
 	l, err := wasip1.Listen("tcp", sdk.WorkAddress)
 	if err != nil {
 		return err
 	}
-	return http.Serve(l, handler)
+	return http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		taskID := r.Header.Get("X-Timecraft-Task")
+		creator := r.Header.Get("X-Timecraft-Creator")
+
+		logger.Printf("executing task %s for process %s", taskID, creator)
+
+		handler.ServeHTTP(w, r)
+	}))
 }
