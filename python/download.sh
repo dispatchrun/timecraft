c7f96107853a6d9deee5c9c26aa23926f6823a15
Thomas Pelletier <thomas@pelletier.codes>
2023-06-30 11:17:29 -0400

    Debug

--- python/download.sh
+++ python/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [ -z "$1" ]; then
     >&2 echo -e "$0 downloads pre-built WebAssembly python from s3.\nProvide timecraft git sha to download."
@@ -13,12 +13,10 @@ python_wasm_out="cpython/python.wasm"
 python_zip_out="cpython/usr/local/lib/python311.zip"
 
 mkdir -p cpython/usr/local/lib/
-curl https://timecraft.s3.amazonaws.com/python/${commit}/python.wasm > ${python_wasm_out}
-curl https://timecraft.s3.amazonaws.com/python/${commit}/python311.zip > ${python_zip_out}
+curl --fail-with-body https://timecraft.s3.amazonaws.com/python/${commit}/python.wasm > ${python_wasm_out}
+curl --fail-with-body https://timecraft.s3.amazonaws.com/python/${commit}/python311.zip > ${python_zip_out}
 
 echo "Python downloaded at:"
 echo ${python_wasm_out}
-cat ${python_wasm_out}
-
 echo ${python_zip_out}
-od -t x1 -N 8 ${python_zip_out}
+
