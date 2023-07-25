e12c208bf62397ff1150ed780a7b1623fb180195
Julien Fabre <ju.pryz@gmail.com>
2023-07-25 08:17:29 -0600

    wip

--- internal/timecraft/engine.go
+++ internal/timecraft/engine.go
@@ -3,11 +3,15 @@ package timecraft
 import "context"
 
 type Engine interface {
-	Run(ctx context.Context, spec Spec)
-}
+	// Create a process but don't start it
+	Create(ctx context.Context, spec ProcessSpec, logSpec *LogSpec) (*ProcessInfo, error)
 
-type Spec struct {
-	Path string
-	Args []string
-	Env  []string
+	// Run a pre-created process
+	Run(ctx context.Context, pid ProcessID) error
+
+	// Wait for a process to exit
+	Wait(pid ProcessID) error
+
+	// Close a process:
+	Close(pid ProcessID) error
 }
