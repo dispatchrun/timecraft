69469d3047972109b19f44d999174bf783468d29
Julien Fabre <ju.pryz@gmail.com>
2023-08-16 18:37:02 -0600

    merge tests

--- testdata/python/app_test.py
+++ testdata/python/app_test.py
@@ -1,9 +1,39 @@
 import unittest
 import asyncio
 
-from .app import app
+import sys
+import asyncio
+import pickle
+
+from timecraft.app import App
+
 
 class TestApp(unittest.IsolatedAsyncioTestCase):
     def test_run(self):
         res = asyncio.run(app.run())
         self.assertEqual(res, 42)
+
+
+
+app = App()
+
+@app.function(name="add")
+def add(a, b):
+    return a + b
+
+
+@app.start()
+async def main():
+    res = add.call(40, 2)
+
+    while not await res.is_done():
+        await asyncio.sleep(0.1)
+
+    resp = await res.result()
+    if resp is not None and resp.output is not None:
+        return pickle.loads(resp.output.body)
+
+
+# TODO: can we move __main__ to the SDK?
+if __name__ == "__main__":
+    asyncio.run(app.run(sys.argv[1:]))
